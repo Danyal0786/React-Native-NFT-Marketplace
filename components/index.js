f7ba2ba05ffc4fb9ab53c70d73ac90207fdc816c
danyal0786 <danyalakhtar1998@gmailcom>
2022-05-09 07:41:29 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,20 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { RectButton, CircleButton } from "./Button";
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+
+export {
+    HomeHeader,
+    RectButton,
+    CircleButton,
+    NFTCard,
+    FocusedStatusBar,
+    DetailsDesc,
+    DetailsBid,
+    SubInfo,
+
+}
